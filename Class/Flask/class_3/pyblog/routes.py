67274e5c36e4c633308f9e57cfc43d156b1fd113
x41lakazam <alakazamjoined@gmail.com>
2019-08-05 20:33:15 +0300

    05/08/2019

---
+++ Class/Flask/class_3/pyblog/routes.py
@@ -0,0 +1,14 @@
+###################################################
+######                                       ######
+######            pyblog/routes.py           ###### 
+######                                       ######
+###################################################
+
+from pyblog import app
+import flask
+
+@app.route('/')
+@app.route('/home')
+def homepage():
+    return "Hello world"
+
