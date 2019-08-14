9ff8916e36c6ce9623e06caed44ba3a756ed55ec
x41lakazam <alakazamjoined@gmail.com>
2019-08-14 21:03:24 +0300

    14/08/2019

--- Class/Flask/classes/class_5/main.py
+++ Class/Flask/classes/class_5/main.py
@@ -4,7 +4,13 @@
 #######                                   ####### 
 #################################################
 
+
+import sys
+print("exe:",sys.executable)
+
 from pyblog import app
+
+
 
 if __name__ == "__main__":
     app.run()
